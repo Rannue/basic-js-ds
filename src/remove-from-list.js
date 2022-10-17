c5ece7a2f2ac22b5f2466159b999fd6c9113c646
rannue <yavianbo@gmail.com>
2022-10-17 23:14:41 +0300

    feat: add solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,10 +22,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
 }
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+let list = convertArrayToList([3, 1, 3, 3, 6, 3, 5])
+
+function removeKFromList(l, k) {
+  let currentNode = l
+  let prevNode = currentNode
+  while (currentNode != null) {
+    if (currentNode.value == k) {
+      let p = prevNode
+      let c = currentNode
+      while (c != null && c.value == k) {
+        c = c.next
+      }
+      p.next = c
+    }
+    prevNode = currentNode
+    currentNode = currentNode.next
+  }
+
+  node = l;
+  return (node.value != k) ? l : node.next
+}
+
+console.log(removeKFromList(list, 3))
 
 module.exports = {
   removeKFromList
